bdf285cf254c49bd41f6a482ceb89671438b9655
vineethjunuri <vineethjunuri@gmail.com>
2022-07-19 13:44:53 +0000

    jobby app

---
+++ src/components/UserProfile/UserProfile.js
@@ -0,0 +1,16 @@
+import './UserProfile.css'
+
+const UserProfile = props => {
+  const {profileInfo} = props
+  return (
+    <>
+      <img
+        src={profileInfo.profileImageUrl}
+        alt="profile"
+        className="profile-pic"
+      />
+      <h1 className="profile-name">{profileInfo.name}</h1>
+      <p className="profile-description">{profileInfo.shortBio}</p>
+    </>
+  )
+}
